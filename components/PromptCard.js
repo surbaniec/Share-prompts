acf57b51d8935c72aebe33f2fc8b0326883e7ede
surbaniec <radsler1999@gmail.com>
2023-05-08 19:31:22 +0200

    Create Profile page, create profile component, fetch current user prompts

--- components/PromptCard.js
+++ components/PromptCard.js
@@ -5,9 +5,10 @@ import Image from 'next/image';
 import { useSession } from 'next-auth/react';
 import { usePathname, useRouter } from 'next/navigation';
 
-const PromptCard = ({ post, handleTagClick }) => {
+const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
   const router = useRouter();
-  const { data } = useSession();
+  const pathName = usePathname();
+  const { data: session } = useSession();
 
   const [copied, setCopied] = useState('');
 
@@ -57,6 +58,22 @@ const PromptCard = ({ post, handleTagClick }) => {
       >
         {post.tag}
       </p>
+      {session?.user.id === post.creator._id && pathName === '/profile' && (
+        <div className='mt-5 flex-center gap-4 border-t border-gray-100 pt-3'>
+          <p
+            className='font=inter text-sm green_gradient cursor-pointer'
+            onClick={handleEdit}
+          >
+            Edit
+          </p>
+          <p
+            className='font=inter text-sm orange_gradient cursor-pointer'
+            onClick={handleDelete}
+          >
+            Delete
+          </p>
+        </div>
+      )}
     </div>
   );
 };
