0e4c977779cd3f6b8785e94207599a7e8268f8df
surbaniec <radsler1999@gmail.com>
2023-05-08 20:00:00 +0200

    Add update & delete prompt

--- app/profile/page.js
+++ app/profile/page.js
@@ -9,6 +9,8 @@ const ProfilePage = () => {
   const { data: session } = useSession();
   const [posts, setPosts] = useState([]);
 
+  const router = useRouter();
+
   useEffect(() => {
     const fetchPosts = async () => {
       const res = await fetch(`/api/users/${session?.user.id}/posts`);
@@ -18,14 +20,33 @@ const ProfilePage = () => {
     if (session?.user.id) fetchPosts();
   }, []);
 
-  const handleEdit = () => {};
+  const handleEdit = (post) => {
+    router.push(`/update-prompt?id=${post._id}`);
+  };
 
-  const handleDelete = async () => {};
+  const handleDelete = async (post) => {
+    const hasConfirmed = confirm(
+      'Are you sure you want to delete this prompt?'
+    );
+
+    if (hasConfirmed) {
+      try {
+        await fetch(`/api/prompt/${post._id.toString()}`, {
+          method: 'DELETE',
+        });
+
+        const filteredPosts = posts.filter((p) => p._id !== post._id);
+        setPosts(filteredPosts);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
 
   return (
     <Profile
       name='My'
-      desc='Welcome to yoiur personalized profilke page'
+      desc='Welcome to your personalized profile page'
       data={posts}
       handleEdit={handleEdit}
       handleDelete={handleDelete}
